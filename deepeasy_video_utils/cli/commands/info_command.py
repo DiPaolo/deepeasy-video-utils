1af48b1db8f9f714c93fad1ded2d263b6d6b3a03
Pavel Ditenbir <pavel.ditenbir@gmail.com>
2024-04-29 10:52:47 +0300

    ability to get video duration for folder

--- deepeasy_video_utils/cli/commands/info_command.py
+++ deepeasy_video_utils/cli/commands/info_command.py
@@ -1,3 +1,4 @@
+import os
 import sys
 
 import click
@@ -15,21 +16,50 @@ def info():
 
 
 @info.command()
-@click.option('-f', '--file', type=str, metavar='filename', help='Get media duration in seconds')
-def duration(file: str):
-    if file is None:
-        logger.error('failed to get duration: file is not specified')
-        click.echo("filename is not specified. Please specify it using '-f' or '--file'", err=True)
+@click.option('-f', '--file', type=str, metavar='filename',
+              help='Get media duration (in seconds) of specific file')
+@click.option('-d', '--dir', type=str, metavar='directory',
+              help='Get media duration (in seconds) for every media in folder')
+def duration(file: str, dir: str):
+    if file is None and dir is None:
+        logger.error('failed to get duration: neither filename nor directory is specified')
+        click.echo("neither filename nor directory is specified. Please specify one of them using "
+                   "'-f'/'--file' or '-d'/'--dir'", err=True)
         sys.exit(GENERATOR_CMD_START_RET_CODE + 0)
+
+    if file and dir:
+        logger.error('failed to get duration: the both file and directory are specified')
+        click.echo("the both file and directory are specified. Please specify exactly one of them", err=True)
+        sys.exit(GENERATOR_CMD_START_RET_CODE + 1)
 
     if config.CALC_PERFORMANCE:
         perf_counter = cli_utils.start_perf_counter(f"get media duration")
 
-    duration_sec = get_video_duration(file)
-    if duration_sec.is_integer():
-        duration_sec = int(duration_sec)
+    def process_file(filename, exit_on_error: bool):
+        duration_sec = get_video_duration(filename, True if exit_on_error else False)
 
-    click.echo(duration_sec)
+        if duration_sec is None:
+            if exit_on_error:
+                err_msg = 'failed to get duration: unknown error'
+                logger.error(err_msg)
+                click.echo(err_msg, err=True)
+                sys.exit(GENERATOR_CMD_START_RET_CODE + 2)
+        else:
+            if duration_sec.is_integer():
+                duration_sec = int(duration_sec)
+
+            if exit_on_error:
+                click.echo(duration_sec)
+            else:
+                click.echo(f'{os.path.basename(filename)}: {duration_sec}')
+
+    if file:
+        process_file(file, True)
+    elif dir:
+        for file in [os.path.join(dir, file) for file in os.listdir(dir) if os.path.isfile(os.path.join(dir, file))]:
+            process_file(file, False)
+    else:
+        pass
 
     if config.CALC_PERFORMANCE:
         cli_utils.print_perf_counter_report(perf_counter)
--- deepeasy_video_utils/services/video_utils.py
+++ deepeasy_video_utils/services/video_utils.py
@@ -1,13 +1,20 @@
 import json
+from typing import Optional
 
 from deepeasy_video_utils.services.process import run_command
 
 
-def get_video_duration(filename: str) -> float:
+def get_video_duration(filename: str, print_error_log: bool=True) -> Optional[float]:
     # ffprobe -v error -show_entries stream=duration -print_format json Best\ Kittycat\ Song\ \[OFFICIAL\]\ feat.\ GRUMPY\ CAT.mp4
     ret = run_command(
         ['ffprobe', '-hide_banner', '-v', 'error', '-show_entries', 'stream=duration', '-print_format', 'json',
          filename])
+
+    if ret.returncode != 0:
+        if print_error_log:
+            print(ret.stderr)
+        return None
+
     json_data = json.loads(ret.stdout)
 
     max_duration = 0.0
